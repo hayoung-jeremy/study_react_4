dc29f87e57adfb83d70bbe769b68dcf0d7168c9c
Ha young, Kim <jeremiah91@naver.com>
2021-06-08 15:49:13 +0900

    TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
 import TodoItem from "../TodoItem";
 
 const TodoListBlock = styled.div`
@@ -10,11 +11,17 @@ const TodoListBlock = styled.div`
 `;
 
 const TodoList = () => {
+  const todos = useTodoState();
   return (
     <TodoListBlock>
-      <TodoItem text="출근" done={true} />
-      <TodoItem text="채은이 만나기" done={false} />
-      <TodoItem text="JIRA-45" done={false} />
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      ))}
     </TodoListBlock>
   );
 };
